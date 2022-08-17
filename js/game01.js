e398ebd7e05a4262f6cbe0cba7b86f2a2f7d8cc8
zhukova_oksana <ksy2905@gmail.com>
2022-08-17 19:48:46 +0300

    edit error

--- js/game01.js
+++ js/game01.js
@@ -3,30 +3,29 @@
 const guessNumber = function () {
   const randomNumber = Math.round(Math.random() * 100);
   console.log(randomNumber);
-  let guess = +prompt('Какое число от 1 до 100 я загадал?');
+  let guess = prompt('Какое число от 1 до 100 я загадал?');
 
 
     while (true) {
 
       switch (true) {
-        case !(Number.isInteger(guess)):
+        case guess === null:
+          return alert('Конец!');
+
+        case !(Number.isInteger(+guess)):
           guess = +prompt('Введи число!');
           break;
 
-        case (String(guess) === '0'):
-          return alert('Конец!');
-
-        case guess > randomNumber:
+        case +guess > randomNumber:
           guess = +prompt('Меньше!');
           break;
 
-        case guess < randomNumber:
+        case +guess < randomNumber:
           guess = +prompt('Больше!');
           break;
 
-        case (guess === randomNumber):
-          alert('Правильно!');
-          break;
+        default:
+          return alert('Правильно!');
 
       }
     }
